373196a530285ceb138862e16f59408586de6ab1
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-03-14 18:03:05 +0200

    completed database

--- database/data_operations.py
+++ database/data_operations.py
@@ -18,6 +18,30 @@ def insert_data(email="", ip=None):
     session.add(e)
     session.commit()
 
+def get_data_with_email(email=""):
+    """ get the data of a specific email """
+    try:
+        data = session.query(Email).filter(Email.email_address == email).all()
+        data_email = None
+        for item in data:
+            data_email = item.email_address
+    except SQLAlchemyError as e:
+        data_email = None
+    return data_email
+
+
+def get_data():
+    """ get the whole data """
+    data_dict = {"email_address": [], "ip": [], "created_date": []}
+    try:
+        data = session.query(Email).all()
+        for item in data:
+            data_dict["email_address"].append(item.email_address)
+            data_dict["ip"].append(item.ip_address)
+            data_dict["created_date"].append(item.created_on)
+    except SQLAlchemyError as e:
+        data_dict = None
+    return data_dict
 def close_session():
     """ remove the session """
     session.close()
--- database/try.py
+++ database/try.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
-from data_operations import insert_data, close_session
+from data_operations import insert_data, close_session, get_data_with_email, get_data
 from sqlalchemy.exc import SQLAlchemyError
 
 try:
-    insert_data("mo@gmail.com", "126.134.5.5")
+    email = get_data_with_email("hass@gmail.com")
+    data = get_data()
+    print(email)
+    print(data)
     close_session()
+
 except SQLAlchemyError as e:
     print("An error occurred while handling the database operation.")
     print(str(e))
